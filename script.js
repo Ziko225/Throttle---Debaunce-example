01970c634609fccebdc8a61059d538e95aa459cc
Nazar <nazar@bychyk.it>
2023-09-17 16:43:57 +0200

    added throttle example

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-const input = document.querySelector("#input");
-const text = document.querySelector("#text");
+// === debounce ===
+const inputD = document.querySelector("#inputD");
+const textD = document.querySelector("#textD");
 
 const debounce = (func, delay) => {
-    let interval;
+    let timeout;
 
     return () => {
-        clearTimeout(interval);
-        interval = setTimeout(func, delay);
+        clearTimeout(timeout);
+        timeout = setTimeout(func, delay);
     };
 };
 
-input.addEventListener("input", debounce(() => text.innerHTML = input.value, 500));
+inputD.addEventListener("input", debounce(() => textD.innerHTML = inputD.value, 500));
+
+
+// === throttle ===
+
+const inputT = document.querySelector("#inputT");
+const textT = document.querySelector("#textT");
+
+let pause = false;
+
+const throttle = (func, delay) => {
+    return () => {
+        if (pause) {
+            return;
+        }
+
+        pause = true;
+
+        setTimeout(() => {
+            func();
+            pause = false;
+        }, delay);
+    };
+};
+
+inputT.addEventListener("input", throttle(() => textT.innerHTML = inputT.value, 500));
